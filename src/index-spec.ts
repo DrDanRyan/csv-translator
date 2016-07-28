50614be056ed6cb216bcfacc5645982c04155638
Dan Ryan <danryanfl@yahoo.com>
2016-07-28 11:49:09 -0400

    initial commit

---
+++ src/index-spec.ts
@@ -0,0 +1,174 @@
+import {CSVTranslator} from './index';
+import {suite, test} from 'mocha-typescript';
+import {expect} from 'chai';
+const csv = new CSVTranslator();
+const csvSrc = './src/test-input.csv';
+const tsvSrc = './src/test-input.tsv';
+const txtSrc = './src/test-input.txt';
+const dest = './src/test-output.txt';
+
+@suite class ReadMethod {
+  @test readCSV(done: Done) {
+    csv.read(csvSrc, (err, data) => {
+      if (err) { return done(err); }
+      expect(data).to.deep.equal([
+        {a: 1, b: 2},
+        {a: 3, b: 4}
+      ]);
+      done();
+    });
+  }
+
+  @test readTSV(done: Done) {
+    csv.read(tsvSrc, (err, data) => {
+      if (err) { return done(err); }
+      expect(data).to.deep.equal([
+        {a: 1, b: 2},
+        {a: 3, b: 4}
+      ]);
+    });
+    done();
+  }
+
+  @test delimiter(done: Done) {
+    csv.read(txtSrc, {delimiter: '\t'}, (err, data) => {
+      if (err) { return done(err); }
+      expect(data).to.deep.equal([
+        {a: 1, b: 2},
+        {a: 3, b: 4}
+      ]);
+    });
+    done();
+  }
+}
+
+
+@suite class CreateReadStreamMethod {
+  @test readStreamCSV(done: Done) {
+    const readable = csv.createReadStream(csvSrc);
+    let counter = 0;
+    readable.on('data', (obj: any) => {
+      if (counter === 0) {
+        counter++;
+        expect(obj).to.deep.equal({a: 1, b: 2});
+      } else {
+        expect(obj).to.deep.equal({a: 3, b: 4});
+      }
+    });
+    readable.on('finish', done);
+  }
+
+  @test readStreamTSV(done: Done) {
+    const readable = csv.createReadStream(tsvSrc);
+    let counter = 0;
+    readable.on('data', (obj: any) => {
+      if (counter === 0) {
+        counter++;
+        expect(obj).to.deep.equal({a: 1, b: 2});
+      } else {
+        expect(obj).to.deep.equal({a: 3, b: 4});
+      }
+    });
+    readable.on('finish', done);
+  }
+
+  @test readStreamDelimiter(done: Done) {
+    const readable = csv.createReadStream(txtSrc, {delimiter: '\t'});
+    let counter = 0;
+    readable.on('data', (obj: any) => {
+      if (counter === 0) {
+        counter++;
+        expect(obj).to.deep.equal({a: 1, b: 2});
+      } else {
+        expect(obj).to.deep.equal({a: 3, b: 4});
+      }
+    });
+    readable.on('finish', done);
+  }
+}
+
+
+@suite class WriteMethod {
+  @test writeEvenColumns(done: Done) {
+    const data = [{a: 1, b: 2}, {a: 3, b: 4}];
+    csv.write(data, dest, err => {
+      if (err) { return done(err); }
+      csv.read(dest, (err2, newData) => {
+        if (err2) { return done(err2); }
+        expect(newData).to.deep.equal(data);
+        done();
+      });
+    });
+  }
+
+  @test writeUnionColumns(done: Done) {
+    const data = [{a: 1}, {a: 3, b: 4}];
+    csv.write(data, dest, err => {
+      if (err) { return done(err); }
+      csv.read(dest, (err2, newData) => {
+        if (err2) { return done(err2); }
+        expect(newData).to.deep.equal([{a: 1, b: ''}, {a: 3, b: 4}]);
+        done();
+      });
+    });
+  }
+
+  @test writeRestrictedColumns(done: Done) {
+    const data = [{a: 1, b: 2}, {a: 3}];
+    csv.write(data, dest, {columns: ['a']}, err => {
+      if (err) { return done(err); }
+      csv.read(dest, (err2, newData) => {
+        if (err2) { return done(err2); }
+        expect(newData).to.deep.equal([{a: 1}, {a: 3}]);
+        done();
+      });
+    });
+  }
+}
+
+@suite class CreateWriteStreamMethod {
+  @test writeStreamEvenColumns(done: Done) {
+    const data = [{a: 1, b: 2}, {a: 3, b: 4}];
+    const writable = csv.createWriteStream(dest).on('finish', () => {
+      csv.read(dest, (err, newData) => {
+        expect(newData).to.deep.equal(data);
+        done();
+      });
+    }).on('error', done);
+    writable.write(data[0]);
+    writable.write(data[1]);
+    writable.end();
+  }
+
+  @test writeStreamDiscoverColumns(done: Done) {
+    const data = [{a: 1, b: 2}, {a: 3, c: 4}];
+    const writable = csv.createWriteStream(dest).on('finish', () => {
+      csv.read(dest, (err, newData) => {
+        expect(newData).to.deep.equal([{a: 1, b: 2}, {a: 3, b: ''}]);
+        done();
+      });
+    }).on('error', done);
+    writable.write(data[0]);
+    writable.write(data[1]);
+    writable.end();
+  }
+
+  @test writeStreamSpecifyColumns(done: Done) {
+    const data = [{a: 1, b: 2}, {a: 3, c: 4}];
+    const writable = csv.createWriteStream(dest, {columns: ['a', 'c']}).on('finish', () => {
+      csv.read(dest, (err, newData) => {
+        expect(newData).to.deep.equal([{a: 1, c: ''}, {a: 3, c: 4}]);
+        done();
+      });
+    }).on('error', done);
+    writable.write(data[0]);
+    writable.write(data[1]);
+    writable.end();
+  }
+}
+
+
+
+interface Done {
+  (err?: Error): void;
+}
---
+++ src/index.ts
@@ -0,0 +1,129 @@
+import * as parse from 'csv-parse';
+import * as stringify from 'csv-stringify';
+import {createReadStream, writeFile, createWriteStream} from 'fs';
+import {extname} from 'path';
+import * as _ from 'lodash';
+
+export class CSVTranslator {
+  read(src: string, cb: ResultCb<any[]>): any;
+  read(src: string, opts: ReadOptions, cb: ResultCb<any[]>): void;
+  read(src: string, arg1: any, arg2?: any) {
+    let readOptions: ReadOptions;
+    let cb: ResultCb<any[]>;
+    if (arg2 === undefined) {
+      readOptions = {};
+      cb = arg1;
+    } else {
+      readOptions = arg1;
+      cb = arg2;
+    }
+    const parseOptions = this.getParseOptions(src, readOptions);
+    createReadStream(src).pipe(parse(parseOptions, cb));
+  }
+
+  createReadStream(src: string, readOptions: ReadOptions = {}): NodeJS.ReadWriteStream {
+    const parseOptions = this.getParseOptions(src, readOptions);
+    return createReadStream(src).pipe(parse(parseOptions));
+  }
+
+  private getParseOptions(src: string, readOptions: ReadOptions): ParseOptions {
+    return {
+      delimiter: readOptions.delimiter || this.getDefaultDelimiter(src),
+      columns: true,
+      auto_parse: true,
+      skip_empty_lines: true,
+      trim: true
+    };
+  }
+
+  private getDefaultDelimiter(filePath: string) {
+    return (extname(filePath) === '.tsv') ? '\t' : ',';
+  }
+
+  write(data: any[], dest: string, cb: ErrorCb): void;
+  write(data: any[], dest: string, opts: WriteOptions, cb: ErrorCb): void;
+  write(data: any[], dest: string, arg1: any, arg2?: any): void {
+    let writeOptions: WriteOptions;
+    let cb: ErrorCb;
+    if (arg2 === undefined) {
+      writeOptions = {};
+      cb = arg1;
+    } else {
+      writeOptions = arg1;
+      cb = arg2;
+    }
+    const stringifyOptions = this.getStringifyOptions(dest, writeOptions, data);
+    const nestedArrayData = this.getNestedArrayData(data, stringifyOptions.columns);
+    stringify(nestedArrayData, stringifyOptions, (err, stringData) => {
+      if (err) { return cb(err); }
+      writeFile(dest, stringData, cb);
+    });
+  }
+
+  createWriteStream(dest: string, opts: WriteOptions = {}): stringify.Stringifier {
+    const stringifyOptions = this.getStringifyOptions(dest, opts);
+    const stringifyTransform = stringify(stringifyOptions);
+    stringifyTransform.pipe(createWriteStream(dest));
+    return stringifyTransform;
+  }
+
+  private getStringifyOptions(dest: string, writeOptions: WriteOptions, data?: any[]): StringifyOptions {
+    const delimiter = writeOptions.delimiter || this.getDefaultDelimiter(dest);
+    const header = true;
+    let columns: string[];
+
+    if (writeOptions.columns) {
+      columns = writeOptions.columns;
+    } else if (data) {
+      columns = this.getDataFields(data);
+    } else {
+      columns = undefined;
+    }
+
+    return {delimiter, header, columns};
+  }
+
+  private getDataFields(data: any[]): string[] {
+    return _(data).flatMap(_.keys).uniq().value() as string[];
+  }
+
+  private getNestedArrayData(data: any[], columns: string[]): any[] {
+    return _.map(data, obj => {
+      return _.map(columns, col => {
+        return obj[col];
+      });
+    });
+  }
+}
+
+
+export interface ReadOptions {
+  delimiter?: string;
+}
+
+export interface WriteOptions {
+  delimiter?: string;
+  columns?: string[];
+}
+
+interface ParseOptions {
+  delimiter: string;
+  columns: boolean;
+  auto_parse: boolean;
+  skip_empty_lines: boolean;
+  trim: boolean;
+}
+
+interface StringifyOptions {
+  delimiter: string;
+  columns?: string[];
+  header: boolean;
+}
+
+export interface ErrorCb {
+  (err?: Error): void;
+}
+
+export interface ResultCb<T> {
+  (err: Error, result?: T): void;
+}
